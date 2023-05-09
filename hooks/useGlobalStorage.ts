944ef23acef8b228a7660141bed2bca2b98e1919
Sangsterr <mrsangster@hotmail.com>
2023-05-09 16:44:03 +0100

    adding hooks and useGlobalStorage

---
+++ hooks/useGlobalStorage.ts
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import { getAsyncStorage, setAsyncStorage } from "../utils/asyncStorage";
+
+// Synchronises AsyncStorage with state, then returns the state value
+function useGlobalStorage(key) {
+  const [value, setValue] = useState(undefined);
+
+  // Set 'value' state to item read from storage
+  const readItemFromStorage = async () => {
+    const item = await getAsyncStorage(key);
+    setValue(item);
+  };
+
+  // Update item in storage to newValue
+  const writeItemToStorage = async (newValue) => {
+    await setAsyncStorage(key, newValue);
+    setValue(newValue);
+  };
+
+  useEffect(() => {
+    // sets new value in state
+    readItemFromStorage();
+  }, []);
+
+  return { value, writeItemToStorage };
+}
+
+export default useGlobalStorage;
